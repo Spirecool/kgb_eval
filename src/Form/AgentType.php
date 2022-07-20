5f111c33a6cb27c198fcde512fad83959514eabe
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2022-07-20 21:27:17 +0200

    CRUD Agent

---
+++ src/Form/AgentType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Agent;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class AgentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('last_name')
+            ->add('first_name')
+            ->add('identification_code')
+            ->add('birthday', DateType::class, [
+                'widget' => 'choice',
+                'format' => 'y-M-d',
+                'years' => range(date("Y") - 90, date("Y") - 18)
+            ])
+            ->add('nationality', ChoiceType::class, [
+                'choices' => [
+                    'Belgium' => 'Belgium',
+                    'Brazil' => 'Brazil',
+                    'China' => 'China',
+                    'Congo' => 'Congo',
+                    'France' => 'France',
+                    'Japan' => 'Japan',
+                    'UK' => 'UK',
+                    'USA' => 'USA',
+                    'Russia' => 'Russia',
+                    'Swiss' => 'Swiss',
+                ]
+            ])
+            ->add('skill')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Agent::class,
+        ]);
+    }
+}
