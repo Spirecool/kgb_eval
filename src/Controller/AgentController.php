7f3130a601eac1bc73d706d2a9a101e5c364c2ec
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2022-07-20 21:38:55 +0200

    CRUD Agent route

--- src/Controller/AgentController.php
+++ src/Controller/AgentController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('admin/agents')]
+#[Route('admin/agent')]
 class AgentController extends AbstractController
 {
     #[Route('/', name: 'app_agent_index', methods: ['GET'])]
