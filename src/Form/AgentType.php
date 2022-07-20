3da2376d64258122daa96b922221651e039c0e8e
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2022-07-20 22:16:48 +0200

    issue with new agent in process

--- src/Form/AgentType.php
+++ src/Form/AgentType.php
@@ -15,28 +15,7 @@ class AgentType extends AbstractType
     {
         $builder
             ->add('last_name')
-            ->add('first_name')
-            ->add('identification_code')
-            ->add('birthday', DateType::class, [
-                'widget' => 'choice',
-                'format' => 'y-M-d',
-                'years' => range(date("Y") - 90, date("Y") - 18)
-            ])
-            ->add('nationality', ChoiceType::class, [
-                'choices' => [
-                    'Belgium' => 'Belgium',
-                    'Brazil' => 'Brazil',
-                    'China' => 'China',
-                    'Congo' => 'Congo',
-                    'France' => 'France',
-                    'Japan' => 'Japan',
-                    'UK' => 'UK',
-                    'USA' => 'USA',
-                    'Russia' => 'Russia',
-                    'Swiss' => 'Swiss',
-                ]
-            ])
-            ->add('skill')
+
         ;
     }
 
