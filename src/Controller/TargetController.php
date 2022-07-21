85e21c9e7f20fccd87b6a3e51299ea6c68236213
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2022-07-21 15:26:22 +0200

    FIX menu

--- src/Controller/TargetController.php
+++ src/Controller/TargetController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/targets')]
+#[Route('admin/targets')]
 class TargetController extends AbstractController
 {
     #[Route('/', name: 'app_target_index', methods: ['GET'])]
